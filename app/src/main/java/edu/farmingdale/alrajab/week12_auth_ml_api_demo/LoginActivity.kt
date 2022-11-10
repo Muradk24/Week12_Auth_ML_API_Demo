86462edccb890e03365c31dbaf2714b15331a813
Murad Khan <Muradak1234@gmail.com>
2022-11-09 22:04:55 -0500

    Initial commit

---
+++ app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LoginActivity.kt
@@ -0,0 +1,106 @@
+package edu.farmingdale.alrajab.week12_auth_ml_api_demo
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import edu.farmingdale.alrajab.week12_auth_ml_api_demo.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
+        binding.googlelogo.setOnClickListener {
+
+
+}
+        binding.signUpTv.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.loginBtn.setOnClickListener{
+            login();
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
+        val idToken = googleCredential.googleIdToken
+        when {
+            idToken != null -> {
+                // Got an ID token from Google. Use it to authenticate
+                // with Firebase.
+                val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                firebaseAuth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success")
+                            val user = firebaseAuth.currentUser
+                            //updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.exception)
+                            updateUI(null)
+                        }
+                    }
+            }
+            else -> {
+                // Shouldn't happen.
+                Log.d(TAG, "No ID token!")
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = firebaseAuth.getCurrentUser()
+     //  updateUI(currentUser);
+    }
+
+    private fun login() {
+        val email = binding.emailET.text.toString()
+        val pass = binding.passET.text.toString()
+
+        if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+            firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val intent = Intent(this, LandingActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        } else {
+            Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+
+
+
+}
