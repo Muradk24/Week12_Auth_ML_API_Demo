38d9c5247cfb502b24d7aee8bb2e4360e19a9592
Murad Khan <Muradak1234@gmail.com>
2022-11-14 21:08:43 -0500

    complete google login

--- app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LoginActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LoginActivity.kt
@@ -2,17 +2,25 @@ package edu.farmingdale.alrajab.week12_auth_ml_api_demo
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import edu.farmingdale.alrajab.week12_auth_ml_api_demo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var oneTapClient: SignInClient
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
     lateinit var binding:ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
@@ -21,6 +29,8 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
+        oneTapClient = Identity.getSignInClient(this)
+
         val signInRequest = BeginSignInRequest.builder()
             .setGoogleIdTokenRequestOptions(
                 BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
@@ -28,13 +38,27 @@ class LoginActivity : AppCompatActivity() {
                     // Your server's client ID, not your Android client ID.
                     .setServerClientId(getString(R.string.web_client_id))
                     // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build())
-            .build()
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()) // Automatically sign in when exactly one credential is retrieved.
+             .build()
         binding.googlelogo.setOnClickListener {
 
-
-}
+            oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this) { result ->
+                    try {
+                        startIntentSenderForResult(
+                            result.pendingIntent.intentSender, REQ_ONE_TAP,
+                            null, 0, 0, 0, null)
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener(this) { e ->
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    Log.d(TAG, e.localizedMessage)
+                }
+        }
         binding.signUpTv.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
@@ -44,40 +68,69 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
-//        val idToken = googleCredential.googleIdToken
-//        when {
-//            idToken != null -> {
-//                // Got an ID token from Google. Use it to authenticate
-//                // with Firebase.
-//                val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-//                firebaseAuth.signInWithCredential(firebaseCredential)
-//                    .addOnCompleteListener(this) { task ->
-//                        if (task.isSuccessful) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "signInWithCredential:success")
-//                            val user = firebaseAuth.currentUser
-//                            //updateUI(user)
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w(TAG, "signInWithCredential:failure", task.exception)
-//                            updateUI(null)
-//                        }
-//                    }
-//            }
-//            else -> {
-//                // Shouldn't happen.
-//                Log.d(TAG, "No ID token!")
-//            }
-//        }
-//    }
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        var currentUser = firebaseAuth.getCurrentUser()
-     //  updateUI(currentUser);
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                            firebaseAuth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this) { task ->
+                                    if (task.isSuccessful) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithCredential:success")
+                                        val user = firebaseAuth.currentUser
+                                        val intent = Intent(this, LandingActivity::class.java)
+                                        startActivity(intent)
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                                        updateUI(null)
+                                    }
+                                }
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token or password!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    when (e.statusCode) {
+                        CommonStatusCodes.CANCELED -> {
+                            Log.d(TAG, "One-tap dialog was closed.")
+                            // Don't re-prompt the user.
+                            showOneTapUI = false
+                        }
+                        CommonStatusCodes.NETWORK_ERROR -> {
+                            Log.d(TAG, "One-tap encountered a network error.")
+                            // Try again or just ignore.
+                        }
+                        else -> {
+                            Log.d(TAG, "Couldn't get credential from result." +
+                                    " (${e.localizedMessage})")
+                        }
+                    }
+                }
+            }
+        }
     }
+
+
 
     private fun login() {
         val email = binding.emailET.text.toString()
